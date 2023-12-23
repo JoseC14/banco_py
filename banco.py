e72dc94ff487be42aaf2ed833a0195d769764396
JoseC14 <josecarlosv8474@gmail.com>
2023-12-23 17:40:38 -0300

    Feito funções de criar conta e usuário

--- banco.py
+++ banco.py
@@ -13,9 +13,14 @@ menu          = """
     (s) Sacar
     (d) Depositar
     (e) Extrato
+    (cu) Criar Usuário
+    (cc) Criar Conta
+    (l) Listar Clientes
+    (l) Listar Contas
     (q) Sair do Sistema
 """
-
+usuarios = []
+contas   = []
 def sacar(valor,saques,LIMITES_SAQUE, /):
     global deposito
     global extrato
@@ -43,6 +48,71 @@ def depositar(*,transacao):
     print("Depósito feito com sucesso!")
     extrato = extrato + "Depósito: R$"+str(transacao)+"\n"
 
+
+def criar_usuario(usuarios):
+    nome                  = input("Digite seu nome: ")
+    data_nascimento       = input("Digite sua data de nascimento: ")
+    
+    while True:
+        cpf = input("Digite seu CPF(somente números): ")
+        if(checa_repetido(cpf,usuarios)):
+            print("CPF Já existe no banco de dados")
+        else:
+            break
+
+        
+    logradouro            = input("Digite seu logradouro: ")
+    numero                = input("Digite o número da sua casa: ")
+    bairro                = input("Digite seu bairro: ")
+    cidade                = input("Agora a cidade em que você reside: ")
+    estado                = input("Estado: ")
+
+    endereco = logradouro + " - " + numero + " - " + bairro + " - " + cidade + " - " + estado
+    
+    usuarios.append(dict({
+        "Nome":nome,
+        "Data de Nascimento": data_nascimento,
+        "CPF":cpf,
+        "Endereço":endereco
+    }))
+
+def checa_repetido(cpf,clientes):
+    if clientes:
+        for k,v in clientes[0].items():
+            if v == cpf:
+                return True
+            else: 
+                return False
+
+def lista_clientes(clientes):
+    for cliente in clientes:
+        for k,v in cliente.items():
+            print(f"{k} : {v}")
+    print("-------------------------------------")
+
+
+def criar_conta(contas):
+    agencia = input("Digite o número da agência: ")
+    numero_conta = input("Digite o número da conta(XXXX): ")
+    while True:
+        cpf = input("Digite o CPF da sua conta cadastrada: ")
+        if(not achaCliente(cpf)):
+            print("CPF não encontrado no banco de dados")
+        else: 
+            break
+
+    contas.append({
+        "Agência: ": agencia,
+        "Número da Conta": numero_conta,
+        "CPF do Usuário" : cpf 
+    })
+
+def achaCliente(cpf):
+    for cliente in usuarios:
+        if cliente["CPF"] == cpf:
+            return True
+        else:
+            return False
 while True:
     print(menu)
     print("Digite uma opção")
@@ -61,6 +131,17 @@ while True:
     elif opcao == "e":
         print("EXTRATO")
         print(extrato)
+    elif opcao == "cu":
+        print("CRIAR USUÁRIO")
+        criar_usuario(usuarios)
+    elif opcao == "cc":
+        print("CRIAR CONTA")
+        criar_conta(contas)
+    elif opcao == "l":
+        lista_clientes(usuarios)
+    elif opcao == "q":
+        print("LISTAR CLIENTES")
+        lista_clientes(usuarios)
     elif opcao == "q":
         break;
     else:
